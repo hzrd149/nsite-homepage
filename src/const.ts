067ded757757d95f9becd673418f6e1a611a36d3
hzrd149 <github@hzrd149.com>
2025-06-19 12:15:14 -0500

    adjust default relays

--- src/const.ts
+++ src/const.ts
@@ -8,7 +8,7 @@ export const DEFUALT_PROFILE_RELAYS = [
 export const DEFAULT_RELAYS = [
   "wss://nostrue.com",
   "wss://relay.damus.io",
-  "wss://purplerelay.com",
+  "wss://relay.nsite.lol",
   "wss://relay.snort.social",
   "wss://nostr.wine",
   "wss://relay.primal.net",
@@ -18,9 +18,5 @@ export const FEATURED_SITES_LIST = {
   kind: 30000,
   pubkey: "1805301ca7c1ad2f9349076cf282f905b3c1e540e88675e14b95856c40b75e33",
   identifier: "E1HkNAWzVQSQBVYfzNTDD",
-  relays: [
-    "wss://nostrue.com",
-    "wss://relay.damus.io",
-    "wss://purplerelay.com/",
-  ],
+  relays: ["wss://nostrue.com", "wss://relay.damus.io"],
 };
